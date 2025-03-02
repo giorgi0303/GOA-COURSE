bd3f3436ecaf1b57eef159fcfd8d3825ac1a9273
giorgi0303 <svanski08@gmail.com>
2025-03-02 16:16:11 +0100

    second commit

---
+++ Day 6/Homework/Homework.py
@@ -0,0 +1,157 @@
+#თითოეულ მათემატიკურ ოპერატორზე (+, -, *, /, %, **, //) შეასრულეთ 5 მაგალითი და დაბეჭდეთ შედეგი
+
+#+
+g = 105
+g += 147
+print(g)
+
+G = 300
+G +=200
+print(G)
+
+g1 =1887
+g1 += 2578
+print(g1)
+
+G1 = 2003
+G1 += 3003
+print(G1)
+
+s = 1969
+s += 2478
+print(s)
+
+#-
+name = 20
+name -= 10
+print(name)
+
+name = 100
+name -= 47
+print(name)
+
+name = 1777
+name -= 415
+print(name)
+
+name = 2003
+name -= 1997
+print(name)
+
+name = 1121
+name -= 1079
+print(name)
+
+#*
+l = 5
+l *= 5
+print(l)
+
+l = 100
+l *= 100
+print(l)
+
+l = 200
+l *= 198
+print(l)
+
+l = 2003
+l *= 2007
+print(l)
+
+l = 1998
+l *= 2008
+print(l)
+
+#/
+p = 100
+p /= 2
+print(p)
+
+p = 50
+p /= 2
+print(p)
+
+p = 1995
+p /= 5
+print(p)
+
+p = 20000
+p /= 100
+print(p)
+
+p = 218
+p /= 2
+print(p)
+
+#%
+num = 120
+num %= 13
+print(num)
+
+num = 200
+num %= 20
+print(num)
+
+num = 50
+num %= 10
+print(num)
+
+num = 1120
+num %= 100
+print(num)
+
+num = 2000
+num %= 1000
+print(num)
+
+#**
+nom = 4
+nom **= 2
+print(nom)
+
+nom = 20
+nom **= 10
+print(nom)
+
+nom = 100
+nom **= 4
+print(nom)
+
+nom = 200
+nom **= 3
+print(nom)
+
+nom = 7
+nom **= 2
+print(nom)
+
+#//
+n = 400
+n //= 100
+print(n)
+
+n = 600
+n //= 3
+print(n)
+
+n = 10
+n //= 2
+print(n)
+
+n = 4
+n //= 2
+print(n)
+
+n = 90
+n //= 20
+print(n)
+
+# განმარტებები:
+# /_ ეს არის float ტიპის გაყოფის ნიშანი რომელიც რაიმე რიცხვს ყოფს ატწილადებად მაგ: 10/2 = 5.0, 10/3 = 3.333333333
+# //_ ეს არის integer (int) ტიპის გაყოფის ნიშანი რომელიც იყოფა მთელ რიცხვზე მაგ: 10/2 = 5, 10/3 = 3
+# %_ ეს არის გაყოფა რომელიც აღიქვამს 0101 ოპერატორად მაგ: 10%2 = 0, 10%3 = 1
+
+
+#Extreme Level
+#7) turtle-ში დახაზეთ სასახლე, კოშკზე უნდა იყოს GOA-ს დროშა, ჯგუფიდან საუკეთესო დავალების ავტორი გახდება მინი ლიდერი#
