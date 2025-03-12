f25aeefd41337fe95cab89dbf7d164061b6c7437
giorgi0303 <svanski08@gmail.com>
2025-03-12 21:58:35 +0100

    commit

---
+++ Day 10/Homework/hw3.py
@@ -0,0 +1,22 @@
+#3) თიოთეული მონაცემთა ტიპისთვის (str,int,float), შექმენით 5 ცვლადი და დაუწერეთ კომენტარი თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი
+
+#str
+name1 = "giorgi"
+name2 = "anastasia"
+name3 = "21"
+name4 = "183"
+name5 = "2003"
+
+#int
+number1 = 21
+number2 = 2003
+number3 = 183
+number4 = 17
+number5 = 267
+
+#float
+num1 = 1.7
+num2 = 1.83
+num3 = 77.8
+num4 = 4.5
+num5 =200.3
