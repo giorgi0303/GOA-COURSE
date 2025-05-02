334051377ddc3a0451e21c7e3a28d1ba93a8dcc0
giorgi0303 <svanski08@gmail.com>
2025-05-03 01:19:14 +0200

    second commit

---
+++ Day 18/Homework/hw4.py
@@ -0,0 +1,8 @@
+#4) მომხმარებელს შემოატანინეთ სახელი და თუ ის თქვენს სახელს ემთხვევა დაბეჭდეთ "coincidence"
+
+user = "giorgi"
+user_input = input("enter name:")
+if user_input == user:
+    print("coincidence")
+else:
+    print("not coincidences")
---
+++ Day 18/Homework/hw5.py
@@ -0,0 +1,7 @@
+#5) მოხმარებელს შემოატანინეთ გამოცდის ქულა და შეინახეთ score ცვლადში ინტეჯერად, თუ ქულა მეტია 70-ზე დაუბეჭდეთ რომ გამოცდა გადალახა "passed" სხვა შემთხვევაში კი რომ ჩაიჭრა "failed"
+
+score =  int(input("enter your score:"))
+if score >= 70:
+    print("exams passed")
+else:
+    print("failed")
